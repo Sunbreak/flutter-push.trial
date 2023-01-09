4c6fce6944b5966ef099e3ba532e4409ecf82ab5
Sunbreak <sunbreak.wang@gmail.com>
2023-01-10 07:26:22 +0800

    feat: add jpush

---
+++ jpush/lib/config.dart
@@ -0,0 +1,11 @@
+import 'package:jpush_flutter/jpush_flutter.dart';
+
+final jpush = JPush();
+
+class JpushConfig {
+  JpushConfig._();
+
+  static const appKey = yourAppKey;
+
+  static const channel = '';
+}
